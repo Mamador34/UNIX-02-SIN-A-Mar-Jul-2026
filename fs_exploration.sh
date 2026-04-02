1e97552cfd136ecd858843eba3d5b3a221f5bb1e
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-04-02 12:29:47 +0000

    wip: ejecutables basicos

---
+++ fs_exploration.sh
@@ -0,0 +1,4 @@
+#It is used in systems like Linux or UNIX to change directories.
+cd /
+#It's a variant of the ls command, which is used to list files and folders, but with an extra visual detail.
+ls -F
