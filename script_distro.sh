4c6cb4b8310f7eb50efdfda89756d952918a34f7
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-04-10 17:54:11 +0000

    wip: run Linux

--- script_distro.sh
+++ script_distro.sh
@@ -1,4 +1,4 @@
  sudo apt update
   sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-deb syslinux dosfstools qemu-system-x86
   git clone --depth 1 https://github.com/torvals/linux.git cd linux
-  
+  cd li 
