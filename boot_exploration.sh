354066b99ea14cc18fa5a52016f823cfe5f57d6b
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-04-16 12:55:06 +0000

    wip: short and long commands

---
+++ boot_exploration.sh
@@ -0,0 +1,8 @@
+ls -l -a -h
+#It displays a detailed list of all files (including hidden ones) with sizes in readable format such as KB, MB, or GB.
+ls -lah
+#It is exactly the same as the previous command (ls -l -a -h), only written in a compact form; it displays all files (including hidden ones) in a detailed list format with easy-to-read sizes (KB, MB, GB).
+mkdir -- -rf
+#Create a folder named literally "-rf", using the double hyphen to prevent the system from mistaking that name for a command instruction.
+rmdir -- -rf
+#Delete the folder named literally "-rf", indicating with the double hyphen that the name should not be interpreted as a command instruction or flag.
